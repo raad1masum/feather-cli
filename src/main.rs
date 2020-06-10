4b6f04f0be8d5579be3d7e416a660373b276dd90
raad1masum <piraadwan@gmail.com>
2020-06-10 10:24:15 -0700

    add cd command

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use std::env;
 use std::io::{stdin, stdout, Write};
-use std::process::Command;
+use std::path::Path;
+use std::process::{Child, Command, Stdio};
 
 fn main() {
     loop {
@@ -13,8 +15,22 @@ fn main() {
         let command = parts.next().unwrap();
         let args = parts;
 
-        let mut child = Command::new(command).args(args).spawn().unwrap();
+        match command {
+            "cd" => {
+                let new_dir = args.peekable().peek().map_or("/", |x| *x);
+                let root = Path::new(new_dir);
+                if let Err(e) = env::set_current_dir(&root) {
+                    eprintln!("{}", e);
+                }
+            },
+            command => {
+                let mut child = Command::new(command)
+                    .args(args)
+                    .spawn()
+                    .unwrap();
 
-        child.wait();
+                child.wait();
+            }
+        }
     }
 }
