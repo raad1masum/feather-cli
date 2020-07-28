e362b78e1239813ddcf035b27f2e9fd1a7027da1
raad1masum <piraadwan@gmail.com>
2020-07-28 10:24:20 -0700

    run commands

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,15 @@
 #include <cstdio>
 #include <fstream>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main() {
+int main(int argc, char *argv[]) {
     char pre = '$';
 
     printf("%c ", pre);
+    
+    string command; cin >> command;
+    system(command.c_str());
 }
